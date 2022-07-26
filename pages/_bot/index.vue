2008052b86e40ca3f5adf084c77c80ab3e625ac4
Brett Bowles <brettbowles@Bretts-MacBook-Pro.local>
2022-07-25 21:15:17 -0400

    got rid of comments

--- pages/_bot/index.vue
+++ pages/_bot/index.vue
@@ -23,7 +23,6 @@ import { coinsByBot } from '~/apollo/queries/fetchCoins.gql'
 export default {
   data() {
     return {
-      // TableData: [],
       Coins: [],
       columnHeadings: [ 'ID', 'Buy Date/Time', 'Sold Date/Time', 'Buy Price', 'Sell Price', 'Current Price', 'Transaction Profit', 'Cumulative Profit', 'Market Profit' ],
       selectedCoin: '',
@@ -32,22 +31,6 @@ export default {
   },
   apollo: {
     $loadingKey: 'loading',
-    // TableData: {
-    //   query: tableData,
-    //   variables() {
-    //     return {
-    //       botName: this.$route.params.bot,
-    //       coinAbbrev: this.selectedCoin,
-    //       username: "kenny"
-    //     }
-    //   },
-    //   skip() {
-    //     return !this.selectedCoin
-    //   },
-    //   error(errors) {
-    //     console.log(errors)
-    //   }
-    // },
     Coins: {
       query: coinsByBot,
       variables() {
