afa9b25179625a2a4622ef1da548e12d254add1d
Brett Bowles <brettbowles@Bretts-MacBook-Pro.local>
2022-07-25 21:16:49 -0400

    deleted chart plugin file

--- plugins/chart.js
+++
@@ -1,9 +0,0 @@
-// import Vue from 'vue'
-// import { Line } from "vue-chartjs/legacy";
-// import { Chart, registerables } from 'chart.js';
-// Chart.register(...registerables);
-// Vue.component('Line', Line)
-// Vue.component('Chart', Chart)
-// Vue.component('registerables', registerables)
-
-// export default { Line }
